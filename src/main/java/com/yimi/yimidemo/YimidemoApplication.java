749eaf732a479e6daa3f2dbff8b72cd3b660323a
xinyu <815512297@qq.com>
2019-12-03 10:53:42 +0800

    壹米Demo：01

---
+++ src/main/java/com/yimi/yimidemo/YimidemoApplication.java
@@ -0,0 +1,13 @@
+package com.yimi.yimidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YimidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YimidemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yimi/yimidemo/YimidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yimi.yimidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YimidemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
